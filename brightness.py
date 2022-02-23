cecb494c06ffe5af97ca6e9b28c9ad53b7d56c53
Luke Cordova <lukethegreat000@gmail.com>
2022-02-23 12:44:14 -0800

    still working on sorting by brightness

--- brightness.py
+++ brightness.py
@@ -1,6 +1,33 @@
 import sys, time
+import numpy as np
 import pygame
 from pygame.locals import *
+
+
+def calcBrightness(character, screen):
+    font = pygame.font.SysFont(None, 20)
+    letter = font.render(character, True, (255, 255, 255))
+    screen.blit(letter, (0, 0))
+    avgB = 0
+    for x in range(20):
+        for y in range(20):
+            px = screen.get_at((x, y))
+            avg = px[0]+px[1]+px[2]
+            avg /= 3
+            avgB += avg
+    avgB /= (20*20)
+    return avgB
+def sortByBrightness(ascii, screen):
+    dtype = [('character', 'S10'), ('brightness', float)]
+    holdArr = []
+    for letter in ascii:
+        screen.fill((0,0,0))
+        holdArr.append((letter, calcBrightness(letter, screen)))
+    bArr = np.array(holdArr, dtype=dtype)
+    bArr = np.sort(bArr, order="brightness")
+    for tup in bArr:
+        print(tup[0])
+    return bArr
 
 pygame.init()
 
@@ -8,7 +35,7 @@ screen = pygame.display.set_mode((500, 500))
 black = (0,0,0)
 white = (255,255,255)
 font = pygame.font.SysFont(None, 20)
-
+ascii = 'diso3-2;` '
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:sys.exit()
@@ -17,15 +44,5 @@ while 1:
 
     time.sleep(1)
     screen.fill(black)
-    letter = font.render('`', True, white)
-    screen.blit(letter, (50, 50))
-    avgB = 0
-    for x in range(20):
-        for y in range(20):
-            px = screen.get_at((x+50, y+50))
-            avg = px[0]+px[1]+px[2]
-            avg /= 3
-            avgB += avg
-    avgB /= (20*20)
-    print(avgB)
+    print(sortByBrightness(ascii, screen))
     pygame.display.flip()
