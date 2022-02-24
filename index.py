6088c4eb26a8610615aa67c0942137936b97dcd0
Luke Cordova <lukethegreat000@gmail.com>
2022-02-23 18:01:25 -0800

    added some comments and cleaned up the organization of the index.py file

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import sys, math
+import sys
 import pygame
 import pygame.camera
 from pygame.locals import *
@@ -6,31 +6,40 @@ from brightness import sortByBrightness
 
 pygame.init()
 pygame.camera.init()
+
+# move this to the top to exit the program before
+# carying out other computations if no camera is found
+camlist = pygame.camera.list_cameras()
+if not camlist:
+    print('no camera found')
+    sys.exit()
+
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-# ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{[]?-_+~<>i!lI^;,":`. '
-# ascii = '1234567890!@#$%^&*()qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,./<>?;:"[]\|()*&^%$#@!-=_+`~ '
-# ascii = '.mM /-w'
-ascii = 'MDON+ov-:/.`    '
-# ascii = '    `./:-vo+NODM'
-# ascii = '@#o-. '
-# ascii = '\u2593\u00D8\u00B0\u00B7 '
-ascii = sortByBrightness(ascii, screen)
 size = width, height = screen.get_width(), screen.get_height()
+
+# instantiating the used asciii characters and sorting them based on brightness
+# ascii = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{[]?-_+~<>i!lI^;,":`. '
+ascii = 'MDON+ov-:/.`    '
+ascii = sortByBrightness(ascii, screen)
+
+# setting initial values of some global variables
 pixelSize = 10
 sizeWhole = int((width * height) / pixelSize)
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 26)
+colors = black, white, green = (0, 0, 0), (255, 255, 255), (0, 255, 26)
 font = pygame.font.SysFont(None, pixelSize)
 offset = .5
-camlist = pygame.camera.list_cameras()
-if camlist:
-    cam = pygame.camera.Camera(camlist[0], size)
-    cam.start()
-    image = cam.get_image()
+grow = False
+shrink = False
 
+# initializing the camera
+cam = pygame.camera.Camera(camlist[0], size)
+cam.start()
+image = cam.get_image()
+
+# resetting the screen to the needed size based off of the offset of each character
 screen = pygame.display.set_mode((width * offset, height * offset))
 
+# myMap maps a number s in range a1-a2 to a range b1-b2
 def myMap(s, a1, a2, b1, b2):
     ans = s-a1
     ans *= b2-b1
@@ -38,13 +47,15 @@ def myMap(s, a1, a2, b1, b2):
     ans /= a2-a1
     return ans + b1
 
-grow = False
-shrink = False
+# main loop
 while 1:
     for event in pygame.event.get():
+        # handle exiting the program
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:sys.exit()
+
+            # handle key inputs for growing and shrinking characters
             if event.key == pygame.K_LEFT:
                 shrink = True
                 grow = False
@@ -57,7 +68,7 @@ while 1:
             elif event.key == pygame.K_RIGHT:
                 grow = False
                 
-
+    # handle growing and shrinking characters depending on keyboard inputs
     if grow and pixelSize < 150:
         pixelSize += 1
         sizeWhole = int((width * height) / pixelSize)
@@ -67,13 +78,14 @@ while 1:
         sizeWhole = int((width * height) / pixelSize)
         font = pygame.font.SysFont(None, pixelSize)
 
-
+    # get a new image from the camera
     if cam.query_image():
         image = cam.get_image(image)
 
-        
+    # fill the screen black
     screen.fill(black)
     
+    # add characters based on brightness of every pixelSize pixel in image
     for x in range(0, width, pixelSize):
         for y in range(0, height, pixelSize):
             try:
@@ -84,6 +96,10 @@ while 1:
                 letter = font.render(character, True, green)
                 screen.blit(letter, (x*offset, y*offset))
             except:
+                # this try except is for some testing procedures
+                # for some reason on different devices pygame will create
+                # a bigger display box than needed causing the program
+                # to error when there it goes outside the images pixels
                 1
                 # print(x, y)
 
